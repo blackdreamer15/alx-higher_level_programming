4e4f73a042240e4eaac5b3b500dec9fd46de3b41
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-04 17:17:44 +0000

    Create 13-insert_node.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_node.c
@@ -0,0 +1,12 @@
+#include "lists"
+
+/**
+ * insert_node - Write a function in C that inserts a number into a sorted
+ * singly linked list.
+ * @head: Head of the linked list
+ * @number: Number to be inserted to node
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+}
