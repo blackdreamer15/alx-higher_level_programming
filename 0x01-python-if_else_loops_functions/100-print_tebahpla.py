17f9b03433fc8298f44ffc449ad90ca06016407a
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-04 13:44:34 +0000

    Fix: Make use of format string instead of f-string

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print(chr(i), end="")
+        print("{0}".format(chr(i)), end="")
     else:
-        print(chr(i - 32), end="")
+        print("{0}".format(chr(i - 32)), end="")
