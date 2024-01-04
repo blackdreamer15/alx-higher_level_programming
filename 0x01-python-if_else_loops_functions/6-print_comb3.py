f8263ee618f13818fa7814e8aa8cc4d08be835aa
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-04 12:56:26 +0000

    Fix: Make use of format string instead of f-string

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(0, 10):
     for j in range(i+1, 10):
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("{0}{1}".format(i, j))
         else:
-            print(f"{i}{j}", end=", ")
+            print(f"{0}{1}".format(i, j), end=", ")
