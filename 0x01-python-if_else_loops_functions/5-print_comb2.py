173d35e3dc1f7418fd634dceca9200b054a17ae4
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-04 13:23:45 +0000

    Fix: Add a leading zero for single-digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 
 for num in range(0, 100):
     if num < 10:
-        print("{0:d}".format(num), end=", ")
+        print("0{0:d}".format(num), end=", ")
     else:
         if num == 99:
             print(f"{num:d}")
