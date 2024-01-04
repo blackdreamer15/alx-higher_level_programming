54a7cf6ddc785028c86e50cec99ceba9c94705d6
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-04 17:53:48 +0000

    Add C code to add numbers into a linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,4 +9,30 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *new_node = NULL;
+	listint_t *current = NULL;
+
+	current = *head;
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (*head == NULL || new_node->n < current->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL && (current->n < new_node->n)
+		&& ((current->next)->n < new_node->n))
+		current = current->next;
+
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
