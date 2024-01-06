ae393c0f35d28419ac64f2a574ea245ebf73817e
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-01-06 22:17:31 +0000

    FIx: Refactor the slicing of the list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,4 +3,4 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    return list(my_list[:idx] + my_list[idx:])
+    return list(my_list[:idx] + my_list[idx + 1:])
