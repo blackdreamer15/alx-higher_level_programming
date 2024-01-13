07bfa7a90cb0c81ec72fa6b3edd9d01b08267d25
blackdreamer15 <bakijessy@gmail.com>
2024-01-12 18:42:45 -0800

    Add a Python script to print out a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end=" ")
+        print("")
